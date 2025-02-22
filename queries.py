f99d262918da592876c2201fd6d24d00b19bb661
alanoud niaf <alanoudniaf@gmail.com>
2025-02-22 21:52:51 +0300

    Done

--- queries.py
+++ queries.py
@@ -1,8 +1,29 @@
 # pylint:disable=C0111,C0103
 
 def order_rank_per_customer(db):
-    pass  # YOUR CODE HERE
+    #begin
+    query = '''
+    SELECT
+        OrderID,
+        CustomerID,
+        OrderDate,
+        RANK() OVER (PARTITION BY CustomerID ORDER BY OrderDate) AS OrderRank
+    FROM Orders;
+    '''
+    return db.execute(query).fetchall()
 
 
 def order_cumulative_amount_per_customer(db):
-    pass  # YOUR CODE HERE
+    #begin
+    query = '''
+    SELECT
+            Orders.OrderID,
+            Orders.CustomerID,
+            Orders.OrderDate,
+            SUM(SUM(OrderDetails.UnitPrice * OrderDetails.Quantity)) OVER(PARTITION BY Orders.CustomerID ORDER BY Orders.OrderDate) OrderCumulativeAmount
+        FROM Orders
+        JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID
+        GROUP BY Orders.OrderID
+        ORDER BY Orders.CustomerID
+    '''
+    return db.execute(query).fetchall()
